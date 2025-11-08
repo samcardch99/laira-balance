c2883875d075a2753f3d5cbb290842acf71af1dc
Samuel Cardenas <samcard1999@gmail.com>
2025-11-08 16:53:08 -0500

    hrefs ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,14 @@ export default {
         darkPink: '#a78270',
       },
       screens: {
+        'thin-screen': { raw: '(max-width: 370px)' },
         'md-vertical': { raw: '(min-width: 768px) and (max-aspect-ratio: 1/1)' },
         'md-custom': '637px',
         'md-custom-lg': { raw: '(min-width: 768px) and (max-width: 1023px)' },
-        'thin-screen': { raw: '(max-width: 370px)' },
+        'md-860px': '860px',
+        '2xl-prev': '1440px', // Define un nuevo breakpoint para un ancho mínimo de 2000px
+        '2xl-custom': '2000px', // Define un nuevo breakpoint para un ancho mínimo de 2000px
+        '3xl-custom': '3200px', // Define un nuevo breakpoint para un ancho mínimo de 3200px
       }
     },
 
