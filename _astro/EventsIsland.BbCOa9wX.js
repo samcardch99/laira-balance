8b68faf020d33862183b97fdb1cb1365cc337412
samcardch99 <samcardch99@gmail.com>
2025-11-10 06:47:46 +0000

    Build: (a2a2aaa) events section created with react correct

--- _astro/EventsIsland.BbCOa9wX.js
+++
@@ -1,1 +0,0 @@
-import{E as p}from"./EventsIsland.BhxtrvnH.js";import"./jsx-runtime.D_zvdyIk.js";import"./index.Db0b2aiq.js";export{p as default};
---
+++ _astro/EventsIsland.DecGVebv.js
@@ -0,0 +1,1 @@
+import{E as p}from"./EventsIsland.CnOXXfwq.js";import"./types.DCie_8Ni.js";import"./index.Db0b2aiq.js";export{p as default};
